8da162eb9bd6337b2963635771939fceabbc910d
ianthemilkman <imilko007@gmail.com>
2023-01-26 17:16:20 -0500

    added comments

--- display_scrapings.py
+++ display_scrapings.py
@@ -7,26 +7,25 @@ from time import time
 from datetime import datetime
 
 
+# Function to strip out the data from the packet received from the webscraper.
 def formatData():
-    print("\nPacket imported.")
     print("\nFormatting data...")
+    # Had problems with website not processing request properly.
+    # This if-statement exits the program if the data is an
+    # unexpected amount, indicating we didn't get the webpage
+    # we were expecting. Also for logging purposes.
     if len(data['hours']['data']) != 16:
-        with open('log.txt', 'a') as f:
-            f.write('\n' + str(datetime.now()) + '::\n')
-            f.write('"Hours" list not the right size. Check website functionality.\n')
+        term = 'Length of "Hours" list was an unexpected value.\nCheck website functionality.'
+        lcd = 'WebscraperError'
+        exceptionHandler(term, lcd)
     else:
         pass
 
+    # Gets the max and min values from each dataset.
     global max_hours
-    try:
-        max_hours = data['hours']['data'][-1]
-    except IndexError:
-        max_hours = 'na'
+    max_hours = data['hours']['data'][-1]
     global min_hours
-    try:
-        min_hours = data['hours']['data'][0]
-    except IndexError:
-        min_hours = 'na'
+    min_hours = data['hours']['data'][0]
     global max_temp
     max_temp = data['temps']['max']
     global min_temp
@@ -48,8 +47,10 @@ def formatData():
     global min_precip
     min_precip = data['precip']['min']
 
+    # Create the list of titles for the data.
     global title_list
     title_list = ['Hours(range)\n','Temperature(F)\n', 'Dewpoint(F)\n', 'Wind(mph)\n', 'Skycover(%)\n', 'Precipitation(%)\n']
+    # Put all the max and min values into a list for iterating over.
     global max_min_list
     max_min_list = [
         f'start:{min_hours}  end:{max_hours}\n',
@@ -61,55 +62,68 @@ def formatData():
     ]
 
 
+# Displays the data on the LCD. Much of the complexity
+# in this code refers to the restart function.
 def displayData():
     print("\nDisplaying...\n")
 
     try:
-        restart_flag = 0
+        restart_flag = 0 # 'restart_flag' is disabled on startup.
         while True:
             for i in range(len(title_list)):
+                # This try - except block checks to see if it is
+                # time to restart the webscraper.
                 try:
+                    # Re-enables the 'restart_permission' flag after the
+                    # restart timeframe has passed. This is necessary because
+                    # the 'main()' function will check to see if we are in the
+                    # restart timeframe. If we are, the program will assume that
+                    # it has already restarted and the 'restart_permission' flag
+                    # will be set to 0.
                     global restart_permission
-                    if datetime.now().minute != 5:
+                    if datetime.now().minute != 5: # Restart timeframe is the entire 5th minute of every hour.
                         restart_permission = 1
                     else:
                         pass
 
+                    # Checks if we are in the restart timeframe and if the permission flag is set.
+                    # If True, the restart_flag is set, signaling a restart.
+                    # First clause of this if-statement may not be necessary, to be considered.
                     if (datetime.now().minute == 5) and (restart_permission == 1):
                         restart_flag = 1
                     else:
                         pass
 
+                    # Checks if restart_flag is set, if it is, 'main()' function restarts.
+                    # GPIO pins are cleaned up first so that they can be set again.
                     if restart_flag == 1:
                         gpio.cleanup()
                         main()
                     else:
                         pass
 
+                    # Displays the title and data on the LCD for 4 seconds.
                     global lcd
                     lcd.clear()
                     lcd.message(title_list[i] + max_min_list[i])
                     print(title_list[i] + max_min_list[i])
                     sleep(4)
 
+                # Cleans everything up in case of Ctrl-c.
                 except KeyboardInterrupt:
                     lcd.clear()
                     gpio.cleanup()
                     return
 
+    # Cleans everything up in case of Ctrl-c.
     except KeyboardInterrupt:
         lcd.clear()
         gpio.cleanup()
         return
 
 
-def main():
-    global restart_permission
-    restart_permission = 1
-    if datetime.now().minute == 5:
-        restart_permission = 0
-    else:
-        pass
+# Initializes the GPIO pins and the LCD display.
+def displaySetup():
     print("\nstarting display...")
 
     try:
@@ -120,38 +134,75 @@ def main():
         lcd.clear()
         print("\ndisplay ready.")
 
-        lcd.message("Starting\nwebscraper...")
-
     except Exception:
+        term = 'Error in LCD setup'
+        lcd = 'LcdSetupError'
+        exceptionHandler(term, lcd)
+
+
+# Gets the IP address of the RPi and displays it on
+# the LCD for easier debugging with SSH. This project
+# was originally developed on a college campus, so a
+# static IP was not possible. This may not be needed if
+# you are able to set a static IP for the RPi.
+def displayIP():
+    global lcd
+    lcd.clear()
+    ip_address = os.popen('hostname -I').read()
+    lcd.message(str(ip_address))
+    print('\nDisplaying IP...')
+
+
+# Function for handling errors. Logs the traceback into a 'log.txt'
+# file, displays the error on the LCD, and then cleans up the GPIO pins.
+def exceptionHandler(term_message, lcd_message):
         with open('log.txt', 'a') as f:
             f.write('\n' + str(datetime.now()) + '::\n')
-            f.write('Error in LCD setup\n')
+            f.write(term_message, '\n')
             f.write(traceback.format_exc())
+        global lcd
+        lcd.clear()
+        lcd.message(lcd_message)
+        sleep(10)
+        lcd.clear()
         gpio.cleanup()
 
 
+def main():
+
+    # Setting the 'restart_permission' flag which will only
+    # allow the program to restart once during the whole minute.
+    global restart_permission
+    restart_permission = 1
+    if datetime.now().minute == 5:
+        restart_permission = 0
+    else:
+        pass
+
+    # Calling the 'displaySetup()' function to initialize the LCD display.
+    displaySetup()
+
+    # Running the webscraper
+    global lcd
+    lcd.message("Starting\nwebscraper...")
+    print("\nStarting webscraper...")
     import webscraper_source as ws
 
-    lcd.clear()
-    print("\nRunning webscraper...")
-    ip_address = os.popen('hostname -I').read()
-    lcd.message(str(ip_address))
+    # Displaying the IP address on the LCD while the program is importing
+    # the data. This is mainly for using SSH when debugging.
+    displayIP()
 
+    # Importing the data from the webscraper. This is not instantaneous.
     global data
     data = ws.main()
-
-
+    # Running the format and display functions. 'displayData()' will run continuously.
     try:
         formatData()
         displayData()
     except Exception:
-        with open('log.txt', 'a') as f:
-            f.write('\n' + str(datetime.now()) + '::\n')
-            f.write('Error in display process\n')
-            f.write(traceback.format_exc())
-            lcd.clear()
-            lcd.message('DataDisplayError')
-            gpio.cleanup()
+        term = 'Error in "display_scrapings.main()" while trying to call "formatData()" and "displayData()".'
+        lcd = 'MainDisplayError'
+        exceptionHandler(term, lcd)
 
 
 if __name__ == "__main__":
